706aa33e930fc9a7bdee2a6df8546fa5e59244dd
lkessels <l@lmpkessels.com>
2026-05-11 17:46:58 +0200

    add: exercise 1.1

---
+++ the_c_programming_language/ch_1/exercise_1_1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+#define CHAR_BUFF 20 // Buffer for characters in name
+
+/// Greet user after receiving the name as input
+char greet() 
+{
+    char name[CHAR_BUFF];
+
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+
+    name[strcspn(name, "\n")] = '\0';
+
+    printf("Hello, %s!\n", name);
+}
+
+// Greet the world
+char hello_world() 
+{
+    char hw[] = "Hello, World!";
+
+    printf("%s\n", hw);
+}
+
+int main(void)
+{
+    greet();
+    hello_world();
+}
