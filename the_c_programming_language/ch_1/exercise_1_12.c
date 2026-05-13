6c67513a9314eeaaaac9c72a7886a31fa68b365a
lkessels <l@lmpkessels.com>
2026-05-13 18:52:39 +0200

    add: exercise 1.12

---
+++ the_c_programming_language/ch_1/exercise_1_12.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define IN 1 // Inside the word
+#define OUT 0 // Outside the word
+
+// Display only one word per sentence as output after receiving multiple
+// words in one sentence as input
+int main(void)
+{
+    int c, state;
+
+    state = OUT;
+    while ((c = getchar()) != EOF) {
+        // Checksum of everything that is not a word and indicates
+        // a new word
+        if (c == ' ' || c == '\t' || c == '\n') {
+            if (state == IN) { // In a new word when left the first word
+                putchar('\n');
+                state = OUT;
+            }
+        } else if (state == OUT) { // Leave the first word and enter a new
+            state = IN;
+            putchar(c);
+        } else {
+            putchar(c);
+        }
+    }
+
+    return 0;
+}
