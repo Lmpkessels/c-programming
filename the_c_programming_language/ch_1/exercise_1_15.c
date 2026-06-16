a63ff472345eea1d05640007d5deb1fbb0f72fa1
lkessels <l@lmpkessels.com>
2026-06-16 18:15:05 +0200

    add: exercise 1.15

---
+++ the_c_programming_language/ch_1/exercise_1_15.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+float celcius(float fahr);
+
+int main(void)
+{
+    float fahr;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+
+    printf("Fahr | Celcius\n");
+    while (fahr <= upper) {
+        printf("%3.0f %6.1f\n", fahr, celcius(fahr));
+        fahr = fahr + step;
+    }
+}
+
+float celcius(float fahr)
+{
+    return (5.0 / 9.0) * (fahr - 32.0);
+}
