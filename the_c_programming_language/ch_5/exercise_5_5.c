1e284f76edba6c41ba0e6cc172336966a8de170a
lkessels <l@lmpkessels.com>
2026-08-25 21:04:06 +0200

    add: exercise 5.5

---
+++ the_c_programming_language/ch_5/exercise_5_5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#define CHAR_BUFF 1000 // Buffer for max amount of characters
+
+// Coppies a specific amount of characters based on 'n'
+void str_n_cpy(char *s, char *t, int n)
+{
+    for (int i = 0; i < n; i++) {
+        s[i] = t[i];
+    }
+}
+
+//TODO: Fix the binary output in print for '\0' such that it becomes normal
+void str_cpy(char *s, char *t) {
+    while (*t != '\0') {
+        *s++ = *t++;
+    }
+}
+
+int main(void)
+{
+    char s[CHAR_BUFF];
+    char *t = "Hello, world!";
+
+    // This will not evaluate to the correct size of characters in t because
+    // t is a pointer a char has the type of 8 so the size of t will be 8
+    int n = sizeof(t) / sizeof(t[0]);
+
+    const int enough_to_get_hello = 5;
+    
+    str_n_cpy(s, t, enough_to_get_hello);
+
+    printf("%d, %s\n", n, s);
+
+    char x[CHAR_BUFF];
+    char *y = "Hello Habibi";
+
+    str_cpy(x, y);
+
+    printf("%s\n", x);
+
+    return 0;
+}
