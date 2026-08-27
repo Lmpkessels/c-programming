71685e021987f6cb49ca3212b8a91c4d06674696
lkessels <l@lmpkessels.com>
2026-08-27 11:21:22 +0200

    add: chapter 5.6 with getline(s, lim)

---
+++ the_c_programming_language/ch_5/exercise_5_6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define CHAR_BUFF 1000 // Maximum amount of characters in 's'
+
+int get_line(char *, int);
+
+int main(void) 
+{
+    char s[CHAR_BUFF];
+    printf("%d\n", get_line(s, CHAR_BUFF));
+}
+
+// Getline is to read a line into s and then return the length
+int get_line(char *s, int lim)
+{
+    int c, i;
+    
+    printf("Type something: ");
+    for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; i++) {
+        *s++ = c;
+    }
+
+    if (c == '\n') {
+        *s++ = c;
+        i++;
+    }
+
+    *s = '\0';
+    return i;
+}
